6167bb1efbb2bcbd4b0fee7924241515a5afa5e9
RUFINO JOHN AGUILAR <aguilarufino@gmail.com>
2019-04-02 21:18:41 +0800

    initial commit - adding user form

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+       <div class="row mt-5">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users</h3>
+
+                <div class="card-tools">
+                    <button class="btn btn-success" data-toggle="modal" data-target="#addnewuserModal">Add New <i class="fas fa-user-plus fa-fw"></i></button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody><tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Modify</th>
+                  </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>
+                        <a href="#">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fas fa-trash text-red"></i>
+                        </a>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addnewuserModal" tabindex="-1" role="dialog" aria-labelledby="addnewuserModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addnewuserModalLabel">New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input v-model="form.name" type="text" name="name" placeholder="Name" class="form-control" :class="{ 'is-invalid': form.errors.has('name')}">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+                    <div class="form-group">
+                        <input v-model="form.name" type="text" name="name" placeholder="Name" class="form-control" :class="{ 'is-invalid': form.errors.has('name')}">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Ceate User</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                form: new Form({
+                    name : '',
+                    email : '',
+                    password : ''
+                })
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
