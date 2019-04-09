2af7c68a57e25d888d95ab175bd1c1208862450b
Dhenn Espiritu <dhenn.espiritu@gmail.com>
2019-04-09 18:29:30 +0800

    edit footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -378,12 +378,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <footer class="main-footer">
                 <!-- To the right -->
                 <div class="float-right d-none d-sm-inline">
-                    Anything you want
+                    Administrator Panel
                 </div>
                 <!-- Default to the left -->
                 <strong
-                    >Copyright &copy; 2014-2018
-                    <a href="https://adminlte.io">AdminLTE.io</a>.</strong
+                    >Copyright &copy; 2019-2020
+                    <a href="">SK Connect</a>.</strong
                 >
                 All rights reserved.
             </footer>
